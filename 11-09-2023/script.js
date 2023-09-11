ec8a32bd2b822267cd2b8362d8dfaf068d24f7ac
MR.Somesh <104584612+SHIVASOMESH@users.noreply.github.com>
2023-09-11 18:31:21 +0530

    Add files via upload

---
+++ 11-09-2023/script.js
@@ -0,0 +1,8 @@
+let ser = document.querySelector(".input");
+
+let search = document.querySelector(".s");
+search.addEventListener("click",function(){
+    ser.classList.toggle("in")
+    search.classList.toggle("ss")
+})
+
